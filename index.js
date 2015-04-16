68bde3ceece71fbd8345ca46043c1faa4d90a025
John Houlder <admin@toxic-productions.com>
2015-04-16 15:33:59 +0100

    Fixed issue #1

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ var token = "";
 module.exports = {
 	token: "",
 	authenticate: function(username,password,callback){
+		var _self = this;
 		request.post({url:"https://api.surl.im/v1/auth",form:{username:username,password:password}},function(err,resp,body){
 			if(!err){
 				if(resp.statusCode == 200){
 					var out = JSON.parse(body);
 					if(out.error == undefined){
-						this.token = out.token;
+						_self.token = out.token;
 						callback();
 					}else{
 						callback(out.error);
